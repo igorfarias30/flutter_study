4b07d818e08ddf75b4dfca48fa35d84294417140
Igor Farias <igorfarias30@hotmail.com>
2020-06-01 20:29:23 -0300

    Ending calculator

--- calculator/lib/components/display.dart
+++ calculator/lib/components/display.dart
@@ -5,7 +5,7 @@ class Display extends StatelessWidget {
   final String text;
 
   Display(this.text);
-  
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -18,19 +18,17 @@ class Display extends StatelessWidget {
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: AutoSizeText(
-                text,
-                minFontSize: 20,
-                maxFontSize: 80,
-                maxLines: 1,
-                textAlign: TextAlign.end,
-                style: TextStyle(
-                  fontWeight: FontWeight.w100,
-                  decoration: TextDecoration.none,
-                  fontSize: 80,
-                  color: Colors.white,
-                )
-                ),
+              child: AutoSizeText(text,
+                  minFontSize: 20,
+                  maxFontSize: 80,
+                  maxLines: 1,
+                  textAlign: TextAlign.end,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w100,
+                    decoration: TextDecoration.none,
+                    fontSize: 80,
+                    color: Colors.white,
+                  )),
             ),
           ],
         ),
--- calculator/lib/models/memory.dart
+++ calculator/lib/models/memory.dart
@@ -1,34 +1,98 @@
 class Memory {
-  static const operations = const['%', '/', 'x', '-', '+', '='];
+  static const operations = const ['%', '/', 'x', '-', '+', '='];
 
   final _buffer = [0.0, 0.0];
   int _bufferIndex = 0;
-  String operation;
+  String _operation;
   String _value = '0';
+  bool _wipeValue = false;
+  String _lastCommand;
 
-  String get value{
+  String get value {
     return _value;
   }
 
-  void applyCommand(String command){
-    if(command == 'AC')
-      this._allClear();
-    else if (operations.contains(command)){
-        _setOperation(command);
-      } else {
-        _addDigit(command);
-      }
+  void applyCommand(String command) {
+    if (_isReplacingOperation(command)) {
+      _operation = command;
+      return;
+    }
+
+    if (command == 'AC')
+      _allClear();
+    else if (operations.contains(command)) {
+      _setOperation(command);
+    } else {
+      _addDigit(command);
+    }
+
+    _lastCommand = command;
   }
 
-  _setOperation(String newOperation){
-    
+  _isReplacingOperation(String command) {
+    return operations.contains(_lastCommand) &&
+        operations.contains(command) &&
+        _lastCommand != '=' &&
+        command != '=';
+  }
+
+  _setOperation(String newOperation) {
+    bool isEqualSign = newOperation == '=';
+
+    if (_bufferIndex == 0) {
+      if (!isEqualSign) {
+        _operation = newOperation;
+        _bufferIndex = 1;
+        _wipeValue = true;
+      }
+    } else {
+      _buffer[0] = _calculate();
+      _buffer[1] = 0.0;
+      _value = _buffer[0].toString();
+      _value = _value.endsWith('.0') ? _value.split('.')[0] : _value;
+
+      _operation = isEqualSign ? null : newOperation;
+      _bufferIndex = isEqualSign ? 0 : newOperation;
+    }
+    _wipeValue = true;
   }
 
   _addDigit(String digit) {
-    _value += digit;
+    final isDot = digit == '.';
+    final wipeValue = (_value == '0' && !isDot) || _wipeValue;
+
+    if (isDot && _value.contains('.') && !wipeValue) return;
+
+    final emptyValue = isDot ? '0' : '';
+    final currentValue = wipeValue ? '' : _value;
+    _value = currentValue + digit;
+    _wipeValue = false;
+
+    _buffer[_bufferIndex] = double.tryParse(_value) ?? 0;
   }
 
-  void _allClear(){
+  void _allClear() {
     _value = '0';
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
+    _wipeValue = false;
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '%':
+        return _buffer[0] % _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case '+':
+        return _buffer[0] + _buffer[1];
+      default:
+        return _buffer[0];
+    }
   }
 }
